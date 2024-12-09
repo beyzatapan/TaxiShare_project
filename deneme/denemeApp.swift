7db8fe21260184b77f83cf83549ff6cb6f2bea36
beyzatapan <beyzatapan@gmail.com>
2024-12-09 19:53:17 +0300

    Initial Commit

---
+++ deneme/denemeApp.swift
@@ -0,0 +1,17 @@
+//
+//  denemeApp.swift
+//  deneme
+//
+//  Created by Beyza Tapan on 9.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct denemeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
